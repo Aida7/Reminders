4e8edef372fac93d421428f4e057d2d50b244764
Садыкова Аида Хаирбекова <@corp.itstep>
2018-05-18 12:47:43 +0600

    Reminders

---
+++ Reminders/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Reminders
+{
+
+    /*Создать приложение-напоминалку.Пользователь может создавать таймер, 
+    который будет запускаться раз в указанное время и предупреждать
+    о каком-то деле, которое тоже задаёт пользователь.*/
+   
+    public partial class MainWindow : Window
+    {
+        int t ;
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void SaveButton_Clik(object sender, RoutedEventArgs e)
+        {
+            Timers timers = new Timers();
+
+            timers.Hour= int.Parse(hour.Text);
+            timers.Sec= int.Parse(sec.Text);
+            timers.Min = int.Parse(hour.Text);
+            
+
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            timers.timer = new TimeSpan(timers.Hour, timers.Min, timers.Sec);
+            dispatcherTimer.Interval = new TimeSpan (timers.Hour, timers.Min, timers.Sec);
+            dispatcherTimer.Tick -= new EventHandler(dispatcherTimer_Tick);
+            
+        }
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+           
+            Content = new Page1();
+            
+        }
+
+    }
+}
---
+++ Reminders/Timers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Reminders
+{
+    public class Timers 
+    {
+        public int Id { get; set; }
+        public DispatcherTimer timer { get; set; }
+        public int Hour { get; set; }
+        public int Min { get; set; }
+        public int Sec { get; set; }
+        public DateTime Time { get; set; }
+    }
+    
+}
